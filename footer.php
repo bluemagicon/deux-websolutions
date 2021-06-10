8747c5e1154941736e7e5486afbe444f18d9c0fd
peverts <phillip@deux.de>
2021-06-10 12:19:23 +0200

    Slick in den Footer

--- footer.php
+++ footer.php
@@ -81,4 +81,7 @@ wp_footer() ;
     AOS.init();
 </script>
 </body>
+
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 </html>
